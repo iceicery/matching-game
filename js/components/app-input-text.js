615f87dc14c15011b31cf5af6c44ffdae2634917
pinwei wu <pw.amy.wu@gmail.com>
2021-03-01 09:30:57 -0600

    adjust layout and font family

--- js/components/app-input-text.js
+++ js/components/app-input-text.js
@@ -13,18 +13,18 @@ class appInputText extends RootElement {
     <form class="form">
     <p class="form__title">Input Text for Matching Game</p>
     <div class="form__textinput-container">
-      <input type="text" id="text1" name="text" placeholder="Enter Text" value='1'>
-      <input type="text" id="text2" name="text" placeholder="Enter Text" value='2'>
-      <input type="text" id="text3" name="text" placeholder="Enter Text" value='3'>
-      <input type="text" id="text4" name="text" placeholder="Enter Text" value='4'>
-      <input type="text" id="text5" name="text" placeholder="Enter Text" value='5'>
-      <input type="text" id="text6" name="text" placeholder="Enter Text" value='6'>
-      <input type="text" id="text7" name="text" placeholder="Enter Text" value='7'>
-      <input type="text" id="text8" name="text" placeholder="Enter Text" value='8'>
-      <input type="text" id="text9" name="text" placeholder="Enter Text" value='9'>
-      <input type="text" id="text10" name="text" placeholder="Enter Text" value='10'>
-      <input type="text" id="text11" name="text" placeholder="Enter Text" value='11'>
-      <input type="text" id="text12" name="text" placeholder="Enter Text" value='12'>
+      <input type="text" id="text1" name="text" placeholder="Enter Text" value='1' class="form__textinput">
+      <input type="text" id="text2" name="text" placeholder="Enter Text" value='2' class="form__textinput">
+      <input type="text" id="text3" name="text" placeholder="Enter Text" value='3' class="form__textinput">
+      <input type="text" id="text4" name="text" placeholder="Enter Text" value='4' class="form__textinput">
+      <input type="text" id="text5" name="text" placeholder="Enter Text" value='5' class="form__textinput">
+      <input type="text" id="text6" name="text" placeholder="Enter Text" value='6' class="form__textinput">
+      <input type="text" id="text7" name="text" placeholder="Enter Text" value='7' class="form__textinput">
+      <input type="text" id="text8" name="text" placeholder="Enter Text" value='8' class="form__textinput">
+      <input type="text" id="text9" name="text" placeholder="Enter Text" value='9' class="form__textinput">
+      <input type="text" id="text10" name="text" placeholder="Enter Text" value='10' class="form__textinput">
+      <input type="text" id="text11" name="text" placeholder="Enter Text" value='11' class="form__textinput">
+      <input type="text" id="text12" name="text" placeholder="Enter Text" value='12' class="form__textinput">
     </div>
     <div>
       <input type="submit" name="submit" class="form__button" value="Confirm">
