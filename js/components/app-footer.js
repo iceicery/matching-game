85ca2e233fcdf44d098b60875c354d6ab19191d9
pinwei wu <pw.amy.wu@gmail.com>
2021-03-02 09:34:44 -0600

    icon no show test

--- js/components/app-footer.js
+++ js/components/app-footer.js
@@ -12,10 +12,10 @@ class appFooter extends RootElement {
         <div class="footer-box">
             <a href="https://iceicery.github.io/pinwei/" target= _blank rel= noreferrer class="footer__text">&copy; Pinwei Wu</a>
             <a href="https://www.linkedin.com/in/pinwei-wu-514713120/" target= _blank rel= noreferrer>
-                 <img src="../images/linkedin.svg" alt="link to linkedin" class="footer__icon"/>
+                 <img src="./images/linkedin.svg" alt="link to linkedin" class="footer__icon"/>
             </a>
             <a href="https://github.com/iceicery" target= _blank rel= noreferrer>
-                <img src="../images/github.svg" alt="link to github" class="footer__icon"/>
+                <img src="./images/github.svg" alt="link to github" class="footer__icon"/>
             </a>
         </div>
         <p class="footer__text">Images from unsplash.com </p>
